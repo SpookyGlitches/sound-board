c5e4b30d2eeb4fc30267ac8117b2dc23dae4c07b
SpookyGlitches <earljseph@gmail.com>
2021-05-14 09:50:58 +0800

    add pages

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 
 const express = require("express");
 const fileUpload = require("express-fileupload");
-const connection = require("./database");
+// const connection = require("./database");
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -14,37 +14,41 @@ app.use(fileUpload());
 
 app.use("/static", express.static("public"));
 
-app.get("/", async (req, res) => {
-	try {
-		let [categories] = await connection
-			.promise()
-			.query("SELECT * FROM categories");
-		let [audios] = await connection
-			.promise()
-			.query(
-				"SELECT * FROM audios a INNER JOIN categories c ON a.category_id = c.category_id"
-			);
-		let array = [];
-
-		categories.forEach((category) => {
-			array.push({
-				category: category,
-				audios: audios.filter(
-					(audio) => audio.title == category.title
-				),
-			});
-		});
-		res.render("index", {
-			array,
-			categories,
-		});
-	} catch (err) {
-		console.log(err);
-		res.send(err);
-	}
+app.get("/", (req, res) => {
+	res.render("explore");
 });
 
-app.use("/audio", require("./routes/audio"));
+// const getAudio = async (req, res) => {
+// 	try {
+// 		let [categories] = await connection
+// 			.promise()
+// 			.query("SELECT * FROM categories");
+// 		let [audios] = await connection
+// 			.promise()
+// 			.query(
+// 				"SELECT * FROM audios a INNER JOIN categories c ON a.category_id = c.category_id"
+// 			);
+// 		let array = [];
+
+// 		categories.forEach((category) => {
+// 			array.push({
+// 				category: category,
+// 				audios: audios.filter(
+// 					(audio) => audio.title == category.title
+// 				),
+// 			});
+// 		});
+// 		res.render("index", {
+// 			array,
+// 			categories,
+// 		});
+// 	} catch (err) {
+// 		console.log(err);
+// 		res.send(err);
+// 	}
+// };
+
+// app.use("/audio", require("./routes/audio"));
 
 app.listen(port, () => {
 	console.log(`App listening in ${port}`);
--- database.js
+++ database.js
@@ -1,18 +1,18 @@
-require('dotenv').config();
-var mysql  = require('mysql2');
+// require('dotenv').config();
+// var mysql  = require('mysql2');
 
-var connection = mysql.createConnection({
-    host:process.env.DB_HOST,
-    user:process.env.DB_USERNAME,
-    password:process.env.DB_PASSWORD,
-    database:process.env.DB_NAME,
-})
-connection.connect(function(err) {
-    if (err) {
-      console.log(err);
-      return console.error('error: ' + err.message);
-    }
-    console.log('Connected to the MySQL server.');
-})
+// var connection = mysql.createConnection({
+//     host:process.env.DB_HOST,
+//     user:process.env.DB_USERNAME,
+//     password:process.env.DB_PASSWORD,
+//     database:process.env.DB_NAME,
+// })
+// connection.connect(function(err) {
+//     if (err) {
+//       console.log(err);
+//       return console.error('error: ' + err.message);
+//     }
+//     console.log('Connected to the MySQL server.');
+// })
 
-module.exports = connection;
+// module.exports = connection;
