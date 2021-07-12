0fc421880e2c0e57ce6957821355e0931eb542f7
SpookyGlitches <earljseph@gmail.com>
2021-07-12 23:07:53 +0800

    add csrf to comments

--- routes/comments.js
+++ routes/comments.js
@@ -1,12 +1,16 @@
 const express = require("express");
+const csrf = require("csurf");
 const router = express.Router({
 	mergeParams: true,
 });
+const csrfProtection = csrf();
 
 const comment = require("../controllers/commentController");
 
 const validate = require("../validations/mw");
 const commentValidation = require("../validations/comment");
+
+router.use(csrfProtection);
 
 router.post("/create", validate(commentValidation), comment.create);
 
