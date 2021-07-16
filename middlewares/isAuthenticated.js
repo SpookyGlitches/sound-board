0516c48b5b93554958753f688fa52020f23483b7
SpookyGlitches <earljseph@gmail.com>
2021-07-17 00:01:23 +0800

    fix somethin

--- middlewares/isAuthenticated.js
+++ middlewares/isAuthenticated.js
@@ -1,5 +1,13 @@
 module.exports = function (req, res, next) {
 	if (req.isAuthenticated()) {
+		if (!req.user.verified_at) {
+			req.flash("errors", [
+				{
+					msg: `Your email address is not verified. Check your email for the verification email or click resend below.`,
+				},
+			]);
+			return res.redirect("/auth/signin");
+		}
 		next();
 	} else {
 		req.flash("You must be signed in to access this resource.");
